c91aa653ce3183be6c40e3cc2ee1a0a3044ba918
Ra <rooba@smoqueed.com>
2021-12-08 13:34:20 -0800

    gitignore and one day maybe touch again

--- test2.py
+++
@@ -1,8 +0,0 @@
-# a = [(z, i,) for z in [(a := 2), (2, 4,), (1, 2,), (3, 4,)] for i in ([1,2,3,4,5,6])]
-a, b = 0, 0
-for z, y in [
-    (d := (a := 2 ** i - 1), ((b := a + (2 * i) & b) % 250)) for i in range(1, 20)
-]:
-    while z > (c := b // 4):
-        z = (b := z ** 2 - 1) >> 0xFF & (y := a ^ b)
-        print(z, c, b, y, a)
